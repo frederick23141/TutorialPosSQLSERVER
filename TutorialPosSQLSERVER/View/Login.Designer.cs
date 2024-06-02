fceb532ecb2febc01098067f7231a981a2afe322
federico toro restrepo <federicotoro23141@gmail.com>
2024-06-02 13:10:47 -0500

    Agregar archivos de proyecto.

---
+++ TutorialPosSQLSERVER/View/Login.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TutorialPosSQLSERVER.View
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hopePictureBox1 = new ReaLTaiizor.Controls.HopePictureBox();
+            this.txt_usuario = new ReaLTaiizor.Controls.SmallTextBox();
+            this.txt_pass = new ReaLTaiizor.Controls.SmallTextBox();
+            this.lostButton1 = new ReaLTaiizor.Controls.LostButton();
+            this.controlBox1 = new ReaLTaiizor.Controls.ControlBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hopePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hopePictureBox1
+            // 
+            this.hopePictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(196)))), ((int)(((byte)(204)))));
+            this.hopePictureBox1.Image = global::TutorialPosSQLSERVER.Properties.Resources.logo;
+            this.hopePictureBox1.Location = new System.Drawing.Point(111, 30);
+            this.hopePictureBox1.Name = "hopePictureBox1";
+            this.hopePictureBox1.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            this.hopePictureBox1.Size = new System.Drawing.Size(175, 175);
+            this.hopePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hopePictureBox1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.hopePictureBox1.TabIndex = 0;
+            this.hopePictureBox1.TabStop = false;
+            this.hopePictureBox1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.BackColor = System.Drawing.Color.Transparent;
+            this.txt_usuario.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.txt_usuario.CustomBGColor = System.Drawing.Color.White;
+            this.txt_usuario.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.txt_usuario.ForeColor = System.Drawing.Color.DimGray;
+            this.txt_usuario.Location = new System.Drawing.Point(84, 314);
+            this.txt_usuario.MaxLength = 32767;
+            this.txt_usuario.Multiline = false;
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.ReadOnly = false;
+            this.txt_usuario.Size = new System.Drawing.Size(250, 28);
+            this.txt_usuario.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            this.txt_usuario.TabIndex = 1;
+            this.txt_usuario.Text = "hg";
+            this.txt_usuario.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_usuario.UseSystemPasswordChar = false;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.BackColor = System.Drawing.Color.Transparent;
+            this.txt_pass.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.txt_pass.CustomBGColor = System.Drawing.Color.White;
+            this.txt_pass.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.txt_pass.ForeColor = System.Drawing.Color.DimGray;
+            this.txt_pass.Location = new System.Drawing.Point(84, 384);
+            this.txt_pass.MaxLength = 32767;
+            this.txt_pass.Multiline = false;
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.ReadOnly = false;
+            this.txt_pass.Size = new System.Drawing.Size(250, 28);
+            this.txt_pass.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            this.txt_pass.TabIndex = 2;
+            this.txt_pass.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_pass.UseSystemPasswordChar = false;
+            // 
+            // lostButton1
+            // 
+            this.lostButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.lostButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lostButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lostButton1.ForeColor = System.Drawing.Color.White;
+            this.lostButton1.HoverColor = System.Drawing.Color.DodgerBlue;
+            this.lostButton1.Image = null;
+            this.lostButton1.Location = new System.Drawing.Point(138, 468);
+            this.lostButton1.Name = "lostButton1";
+            this.lostButton1.Size = new System.Drawing.Size(120, 40);
+            this.lostButton1.TabIndex = 9;
+            this.lostButton1.Text = "lostButton1";
+            // 
+            // controlBox1
+            // 
+            this.controlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.controlBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.controlBox1.CloseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.controlBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.controlBox1.DefaultLocation = true;
+            this.controlBox1.EnableHoverHighlight = true;
+            this.controlBox1.EnableMaximizeButton = false;
+            this.controlBox1.EnableMinimizeButton = true;
+            this.controlBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.controlBox1.Location = new System.Drawing.Point(325, 18);
+            this.controlBox1.MaximizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(74)))), ((int)(((byte)(74)))));
+            this.controlBox1.MinimizeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.controlBox1.Name = "controlBox1";
+            this.controlBox1.Size = new System.Drawing.Size(90, 25);
+            this.controlBox1.TabIndex = 10;
+            this.controlBox1.Text = "controlBox1";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(409, 551);
+            this.Controls.Add(this.controlBox1);
+            this.Controls.Add(this.lostButton1);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.hopePictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.hopePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Controls.HopePictureBox hopePictureBox1;
+        private ReaLTaiizor.Controls.SmallTextBox txt_usuario;
+        private ReaLTaiizor.Controls.SmallTextBox txt_pass;
+        private ReaLTaiizor.Controls.LostButton lostButton1;
+        private ReaLTaiizor.Controls.ControlBox controlBox1;
+    }
+}
